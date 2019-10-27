c73d47ef33999e401a4302ac09d94ffb3223193c
elye <elye.project@gmail.com>
2019-10-27 13:37:43 +1100

    Initial commit

---
+++ app/src/main/java/com/elyeproj/daggertwomultibindings/subcomponentthroughcomponent/SubcomponentThroughModule.kt
@@ -0,0 +1,61 @@
+package com.elyeproj.daggertwomultibindings.subcomponentthroughcomponent
+
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import dagger.Subcomponent
+import javax.inject.Singleton
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Scope
+
+class Data(private val code: Int, private val message: String) {
+    override fun toString(): String {
+        return "$code: $message"
+    }
+}
+
+class RequestHandler {
+    fun writeResponse(data: Data) {
+        println(data)
+    }
+}
+
+@Singleton
+class RequestRouter @Inject constructor(private val requestComponentProvider: Provider<RequestComponent.Builder>) {
+    fun dataReceived(data: Data) {
+        val requestComponent = requestComponentProvider.get()
+            .build()
+        requestComponent.requestHandler.writeResponse(data)
+    }
+}
+
+@Module
+class RequestModule {
+    @Provides
+    fun getRequestHandler() = RequestHandler()
+}
+
+@Subcomponent(modules = [RequestModule::class])
+interface RequestComponent {
+    val requestHandler: RequestHandler
+
+    @Subcomponent.Builder
+    interface Builder {
+        fun build(): RequestComponent
+    }
+}
+
+@Singleton
+@Component
+interface ServerComponent {
+    val requestRouter: RequestRouter
+    val requestComponentBuilder: RequestComponent.Builder
+}
+
+
+fun main() {
+    val a = DaggerServerComponent.create().requestRouter
+    a.dataReceived(Data(200, "My Message"))
+    a.dataReceived(Data(400, "Your Message"))
+}
